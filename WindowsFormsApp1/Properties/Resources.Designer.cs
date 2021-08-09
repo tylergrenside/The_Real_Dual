5696852311e0a42dd51a2f691bbbb17531d1bec4
Tyler Grenside <tylergrenside@mmc.school.nz>
2021-08-10 09:44:12 +1200

    need to make sword move

--- WindowsFormsApp1/Properties/Resources.Designer.cs
+++ WindowsFormsApp1/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace WindowsFormsApp1.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Benny {
+            get {
+                object obj = ResourceManager.GetObject("Benny", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Fireball {
             get {
                 object obj = ResourceManager.GetObject("Fireball", resourceCulture);
